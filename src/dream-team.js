7ee831ac9d256560e7a1b248c12665ebf42a9496
Yauheni Panasevich <www.ep965@gmail.com>
2020-11-01 23:54:30 +0300

    feat: add solutions

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
+  
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    let result = '';
+    if (Array.isArray(members)) {
+        members.forEach((e) => {
+            if (typeof e === 'string') {
+                result += e.replace(/ /g, '')[0];
+            }
+        })
+        if (result === '') { return false; }
+        return result.toUpperCase().split('').sort().join('');
+    } else { return false; }
+
 };
